b1de9b9e928104e0d8124b49442f521731be67f4
Nguyen Tang Bao An <baoan.nguyentang@foodycorp.vn>
2020-03-09 15:49:12 +0700

    First blog post

---
+++ src/posts/2020-03-09-new-blog-post.md
@@ -0,0 +1,7 @@
+---
+path: "/hello-world"
+date: 2020-03-09
+title: "My First Gatsby Post"
+---
+
+Oooooh-weeee, my first blog post!
---
+++ src/templates/post.js
@@ -0,0 +1,26 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+export default function Template({ data }) {
+  const { markdownRemark: post } = data
+  return (
+    <Layout>
+      <SEO title={post.frontmatter.title} />
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query BlogPostByPath($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        path
+        title
+      }
+    }
+  }
+`
